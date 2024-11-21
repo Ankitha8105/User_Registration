562c211fea17f1ab921e602e8906a58830ad2f63
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-21 17:12:43 +0530

    [Ankitha] Add:python program to validate to test valid_lastname function

--- test_UserRegistration.py
+++ test_UserRegistration.py
@@ -13,3 +13,7 @@ import UserRegistration
 @pytest.mark.parametrize("fname,expected",[("Ankitha",True),('Pooja',True),('An',False),('someone',False)])
 def test_fname_valid(fname,expected):
     assert UserRegistration.valid_firstname(fname) == expected
+
+@pytest.mark.parametrize("lname,expected",[("Reddy",True),('kumar',False),('Ab',False)])
+def test_lname_valid(lname,expected):
+    assert UserRegistration.valid_lname(lname) == expected
