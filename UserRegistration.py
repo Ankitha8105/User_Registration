440980f3e72c968644a5974ca57dbece4a1e8f8f
Ankitha8105 <ankithabl2001@gmail.com>
2024-11-21 17:11:49 +0530

    [Ankitha] Add:python program to validate last  name

--- UserRegistration.py
+++ UserRegistration.py
@@ -30,11 +30,34 @@ def valid_firstname(fname):
     except Exception as e:
         log.error("Invalid! error in name")
         return False
+    
+def valid_lname(l_name):
+    """
+    Description:
+        This function checks if user entered valid last name
+    Parameter:
+        l_name: the name to be checked
+    Returns:
+        None
+    """
+    try:
+        if (re.match('^[A-Z][a-z]{2,}',l_name)):
+            log.info("Valid Last name")
+            return True
+        else:
+            log.info("Invalid !,please make sure first letter in upper case and minimum of 3 letters")
+            return False
+        
+    except Exception as e:
+        log.error("Some error occured")
+        return False
 
 def main():
     first_name =input("Enter your First Name :")
     valid_firstname(first_name)
-      
+    last_name = input("Enter your last name :")
+    valid_lname(last_name)
+
 if __name__ == "__main__":
     main()
     
